246865c99da4bd33194669b9739a7e30475717aa
farhan687 <farhan687@gmail.com>
2016-11-16 18:43:56 +0530

    Build

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var rename = require("gulp-rename");
+
+var files = {
+    css: {
+        vendor: [
+            'node_modules/reactive-maps/dist/css/vendor.min.css',
+            'node_modules/reactive-maps/dist/css/style.min.css'
+        ]
+    },
+    js: {
+        vendor: [
+            'node_modules/reactive-maps/dist/js/vendor.min.js'
+        ],
+        custom: [
+        ]
+    }
+};
+
+gulp.task('vendorcss', function() {
+    return gulp.src(files.css.vendor)
+        .pipe(concat('vendor.min.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('vendorjs', function() {
+    return gulp.src(files.js.vendor)
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('compact', [
+    'vendorcss', 
+    'vendorjs'
+]);
+
+gulp.task('default', ['compact']);
